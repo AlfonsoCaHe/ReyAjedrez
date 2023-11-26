d913a1a315f910e9f76aa5bbf3ea7caa3b2d21a2
alfon <alfonsoalgr@gmail.com>
2023-11-26 20:54:07 +0100

    CE 2.D

--- src/main/java/org/iesalandalus/programacion/reyajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/Posicion.java
@@ -1,6 +1,30 @@
 package org.iesalandalus.programacion.reyajedrez;
 
+import java.lang.IllegalArgumentException;
+
 public class Posicion {
     private int fila;
     private char columna;
+
+    public int getFila(){
+        return fila;
+    }
+
+    public char getColumna(){
+        return columna;
+    }
+
+    public void setFila(int fila){
+        if((fila < 1) || (fila > 8)){
+            throw new IllegalArgumentException("La posición del Rey es incorrecta");
+        }
+        this.fila=fila;
+    }
+
+    public void setColumna(char columna){
+        if(columna > 'h'){
+            throw new IllegalArgumentException("La posición del Rey es incorrecta");
+        }
+        this.columna=columna;
+    }
 }
