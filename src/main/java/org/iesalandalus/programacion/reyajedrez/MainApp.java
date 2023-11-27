7d261ca6269084d5b52e0a87d2ba90b9dfaa8750
alfon <alfonsoalgr@gmail.com>
2023-11-27 12:42:53 +0100

    CE 2.G

--- src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
@@ -1,6 +1,10 @@
 package org.iesalandalus.programacion.reyajedrez;
 
+import org.iesalandalus.programacion.reyajedrez.modelo.Rey;
+import org.iesalandalus.programacion.utilidades.Entrada;
+
 public class MainApp {
+    private static Rey rey;
     public static void main(String[] args) {
 
     }
