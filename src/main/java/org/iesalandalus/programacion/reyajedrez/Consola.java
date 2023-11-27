eb21d016c2a810de2263ab147e628311cf143e90
alfon <alfonsoalgr@gmail.com>
2023-11-27 12:23:41 +0100

    CE 2.E

--- src/main/java/org/iesalandalus/programacion/reyajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/Consola.java
@@ -5,6 +5,12 @@ public class Consola {
 
     }
 
-
+    public static void mostrarMenú(){
+        System.out.println("Opciones:");
+        System.out.println("1.Crear rey por defecto.");
+        System.out.println("2.Crear rey eligiendo color.");
+        System.out.println("3.Mover.");
+        System.out.println("4.Salir.");
+    }
 
 }
